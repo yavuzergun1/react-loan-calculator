24c387110be60257939b316de7b21872b0532a5b
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-09-15 21:46:57 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const kkdf = 342;
+  const bsmv = 228;
+  const faizOranı = 0.0228;
+  const kkdfOranı = kkdf / 100000;
+  const bsmvOranı = bsmv / 100000;
+  const toplamfaizOranı = faizOranı + kkdfOranı + bsmvOranı;
+  const taksitSayisi = 12;
+  const krediTutari = 100000;
+  const taksitTutari =
+    krediTutari *
+    ((toplamfaizOranı * (1 + toplamfaizOranı) ** taksitSayisi) /
+      ((1 + toplamfaizOranı) ** taksitSayisi - 1));
+  const faiz = krediTutari * ((faizOranı / 12) * 12);
+  const anaPara = taksitTutari - faiz - kkdf - bsmv;
+  const kalanAnaPara = krediTutari - anaPara;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>aylık ödeme : {taksitTutari.toFixed(2)} </div>
+      <div>faiz {faiz.toFixed(2)}</div>
+      <div>anapara {anaPara.toFixed(2)}</div>
+      <div>kalanAnaPara {kalanAnaPara.toFixed(2)}</div>
     </div>
   );
 }

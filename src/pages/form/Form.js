1d51c6d41e68d5cc4d1dd954a2c7d256b5c286dc
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-09-16 11:44:58 +0300

    form validations done

--- src/pages/form/Form.js
+++ src/pages/form/Form.js
@@ -4,102 +4,138 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
 import Box from "@mui/material/Box";
-import FormControl, { useFormControl } from "@mui/material/FormControl";
+import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import { useFormik } from "formik";
 import validationSchema from "./Validations";
-function Form() {
-  const { handleSubmit, handleChange, values, handleBlur, errors, touched } =
-    useFormik({
-      initialValues: {
-        krediTutari: "",
-        taksitSayisi: "",
-        odemeTipi: "",
-      },
 
-      onSubmit: (values) => {
-        console.log(values);
-        console.log(values.krediTutari);
-      },
-    });
+function Form() {
+  const { handleSubmit, handleChange, values, errors, touched } = useFormik({
+    initialValues: {
+      krediTutari: "",
+      taksitSayisi: "",
+      odemeTipi: "",
+      faizOrani: "",
+      bsmv: "",
+      kkdf: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema,
+  });
 
   return (
     <div>
       <div className="forms-container">
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{
-            "& > :not(style)": { m: 2, width: "28ch" },
-          }}
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-basic"
-            label="Kredi Tutarı"
-            name="krediTutari"
-            variant="outlined"
-            required
-            error={
-              touched.krediTutari && values.krediTutari < 1000 ? true : false
-            }
-            helperText={
-              touched.krediTutari && values.krediTutari < 1000
-                ? "Kredi Tutarı En Az 1000TL'dir"
-                : ""
-            }
-            type="number"
-            // InputProps={{ inputProps: { min: 1000 } }}
-            onChange={handleChange}
-            value={values.krediTutari}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Taksit Sayısı"
-            name="taksitSayisi"
-            variant="outlined"
-            type="number"
-            value={values.taksitSayisi}
-            InputProps={{ inputProps: { max: 24 } }}
-            error={
-              touched.taksitSayisi && values.taksitSayisi < 1 ? true : false
-            }
-            onChange={handleChange}
-            required
-          />
-          <button type="onSubmit">Submit</button>
-        </Box>
         <FormControl>
-          <FormLabel id="demo-row-radio-buttons-group-label">
-            Ödeme Tipi
-          </FormLabel>
-          <RadioGroup
-            row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="odemeTipi"
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              "& > :not(style)": { m: 2, width: "28ch" },
+            }}
+            autoComplete="off"
           >
-            <FormControlLabel
-              value="haftalik"
-              control={<Radio />}
-              label="haftalık Ödeme"
-              onChange={handleChange}
-              name="haftalik"
-            />
-            <FormControlLabel
-              value="aylik"
-              control={<Radio />}
-              label="Aylık Ödeme"
-              onChange={handleChange}
-              name="aylik"
-            />
-            <FormControlLabel
-              value="yillik"
-              control={<Radio />}
-              label="Yıllık Ödeme"
-              onChange={handleChange}
-              name="yillik"
-            />
-          </RadioGroup>
+            <Box
+              sx={{
+                "& > :not(style)": { m: 2, width: "28ch" },
+              }}
+            >
+              <FormLabel id="demo-row-radio-buttons-group-label">
+                Ödeme Tipi
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="odemeTipi"
+              >
+                <FormControlLabel
+                  value="haftalik"
+                  control={<Radio />}
+                  label="haftalık Ödeme"
+                  onChange={handleChange}
+                  name="odemeTipi"
+                />
+                <FormControlLabel
+                  value="aylik"
+                  control={<Radio />}
+                  label="Aylık Ödeme"
+                  onChange={handleChange}
+                  name="odemeTipi"
+                />
+                <FormControlLabel
+                  value="yillik"
+                  control={<Radio />}
+                  label="Yıllık Ödeme"
+                  onChange={handleChange}
+                  name="odemeTipi"
+                />
+                <div style={{ color: "red" }}>
+                  {touched.odemeTipi && errors.odemeTipi}
+                </div>
+              </RadioGroup>
+              <TextField
+                id="outlined-basic"
+                label="Kredi Tutarı"
+                name="krediTutari"
+                variant="outlined"
+                error={touched.krediTutari && errors.krediTutari ? true : false}
+                helperText={errors.krediTutari}
+                type="number"
+                onChange={handleChange}
+                value={values.krediTutari}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Taksit Sayısı"
+                name="taksitSayisi"
+                variant="outlined"
+                type="number"
+                value={values.taksitSayisi}
+                InputProps={{ inputProps: values.odemeTipi == "aylik" ?  { max: 24 } : (values.odemeTipi == "yillik" ?  { max: 2 } :  { max: 104 }) }}
+                error={
+                  touched.taksitSayisi && errors.taksitSayisi ? true : false
+                }
+                helperText={errors.taksitSayisi}
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Faiz Oranı"
+                name="faizOrani"
+                variant="outlined"
+                error={touched.faizOrani && errors.faizOrani ? true : false}
+                helperText={errors.faizOrani}
+                type="number"
+                onChange={handleChange}
+                value={values.faizOrani}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Bsmv Vergi Oranı"
+                name="bsmv"
+                variant="outlined"
+                error={touched.bsmv && errors.bsmv ? true : false}
+                helperText={errors.bsmv}
+                type="number"
+                onChange={handleChange}
+                value={values.bsmv}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Kkdf Vergi Oranı"
+                name="kkdf"
+                variant="outlined"
+                error={touched.kkdf && errors.kkdf ? true : false}
+                helperText={errors.kkdf}
+                type="number"
+                onChange={handleChange}
+                value={values.kkdf}
+              />
+              <button type="onSubmit">Submit</button>
+            </Box>
+          </Box>
         </FormControl>
       </div>
     </div>
--- src/pages/form/Validations.js
+++ src/pages/form/Validations.js
@@ -3,10 +3,20 @@ import * as yup from "yup";
 import { object, string, number, date, InferType, ref } from "yup";
 
 const Validations = yup.object().shape({
-  // Bunlar signUp componentinde bulunan initial values kısmındaki email, password, confirmPassword ile eşleşiyor
-  krediTutari: number().required(),
-  taksitSayisi: number().min(0).required() /* minimum 5 karakter olması şartı */,
-  odemeTipi: string().required() ,
+  krediTutari: yup
+    .number()
+    .min(1000, "Kredi Tutarı En Az 1000TL olmalı")
+    .max(100000, "Maksimum Kredi Tutarınız 100.000TL'dir")
+    .required("Bu Alanı Boş Bırakmayınız"),
+  odemeTipi: yup.string().required("Lütfen Ödeme Tipini Giriniz"),
+  taksitSayisi: yup
+    .number()
+    .min(1, "Lütfen Geçerli Bir Değer Giriniz")
+    .max(12, "En yüksek taksit tutarı 12'dir")
+    .required("Bu Alanı Boş Bırakmayınız"),
+  faizOrani: yup.number().required("Bu Alanı Boş Bırakmayınız"),
+  bsmv: yup.number().required("Bu Alanı Boş Bırakmayınız"),
+  kkdf: yup.number().required("Bu Alanı Boş Bırakmayınız"),
 });
 
 export default Validations;
